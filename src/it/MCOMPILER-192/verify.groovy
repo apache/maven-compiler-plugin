81de779e886adfb75b804fca0ab3e2e36724380b
Hervé Boutemy <hboutemy@apache.org>
2022-12-18 04:53:54 +0100

    [MCOMPILER-516] upgrade plexus-compiler to improve compiling message

--- src/it/MCOMPILER-192/verify.groovy
+++ src/it/MCOMPILER-192/verify.groovy
@@ -23,7 +23,7 @@ assert logFile.exists()
 def content = logFile.getText('UTF-8')
 
 def causedByExpected = content.contains ( 'Caused by: org.apache.maven.plugin.compiler.CompilationFailureException: Compilation failure' )
-def twoFilesBeingCompiled = content.contains ( '[INFO] Compiling 2 source files to ' )
+def twoFilesBeingCompiled = content.contains ( '[INFO] Compiling 2 source files with javac ' )
 def checkResult = content.contains ( '[INFO] BUILD FAILURE' )
 def compilationFailure1 = content.contains( '[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:')
 
