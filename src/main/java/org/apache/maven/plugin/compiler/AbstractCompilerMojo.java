c3473c790f159be5913ac2c953ea00ec32fdb455
Matthias Bünger <mbuenger@apache.org>
2025-06-26 17:45:34 +0200

    Starting compiler plugin annotation processing

--- src/main/java/org/apache/maven/plugin/compiler/AbstractCompilerMojo.java
+++ src/main/java/org/apache/maven/plugin/compiler/AbstractCompilerMojo.java
@@ -278,20 +278,26 @@ public abstract class AbstractCompilerMojo implements Mojo {
     protected String compilerArgument;
 
     /**
-     * Whether annotation processing is performed or not.
-     * If not set, both compilation and annotation processing are performed at the same time.
+     * Configures if annotation processing and/or compilation are performed by the compiler.
      * If set, the value will be appended to the {@code -proc:} compiler option.
-     * Standard values are:
+     *
+     * Possible values are:
      * <ul>
-     *   <li>{@code none} – no annotation processing is performed.</li>
+     *   <li>{@code none} – no annotation processing is performed, only compilation is done.</li>
      *   <li>{@code only} – only annotation processing is done, no compilation.</li>
-     *   <li>{@code full} – annotation processing and compilation are done.</li>
+     *   <li>{@code full} – annotation processing followed by compilation is done.</li>
      * </ul>
      *
-     * Prior Java 21, {@code full} was the default.
-     * Starting with Java 21, the default is {@code none} unless another processor option is used.
+     * The default value depends on the JDK used for the build.
+     * Prior to Java 22, the default was {@code full}, rso annotation processing and compilation were executed without explicit configuration.
+     *
+     * For security reasons, starting with Java 23 no annotation processing is done if neither
+     * any {@code -processor}, {@code -processor path} or {@code -processor module} are set, or either {@code only} or {@code full} is set.
+     * So literally the default is {@code none}.
+     * It is recommended to always provide the annotation processors you want to execute instead of using the {@code proc} configuration, to ensure that only desired processors are executed and not any "hidden" (and maybe malicious).
      *
      * @see #annotationProcessors
+     * @see <a href="https://inside.java/2024/06/18/quality-heads-up/">Inside Java 2024-06-18 Quality Heads up</a>
      * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/man/javac.html#option-proc">javac -proc</a>
      * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/man/javac.html#annotation-processing">javac Annotation Processing</a>
      * @since 2.2
