e96d81920c4227dc03382aa94bf252ce95d1af29
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2026-07-29 16:12:02 +0200

    Document implicitly compiled excluded sources. This is a port of 8d3d6acb09c5a73272c165c6e8a9c53099707296 from 3.x.

--- src/main/java/org/apache/maven/plugin/compiler/CompilerMojo.java
+++ src/main/java/org/apache/maven/plugin/compiler/CompilerMojo.java
@@ -83,6 +83,11 @@ public class CompilerMojo extends AbstractCompilerMojo {
 
     /**
      * A set of exclusion filters for the compiler.
+     * Excluding a source file only prevents the plugin from passing it explicitly to the compiler.
+     * The compiler may still find it on the source path and generate a class file for it.
+     * This can happen with {@code module-info.java}.
+     *
+     * @see #implicit
      */
     @Parameter
     protected Set<String> excludes;
