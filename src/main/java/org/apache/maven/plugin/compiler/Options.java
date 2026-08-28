9319a4ff65e59bd730f5bd1448d019db898d5040
Elliotte Rusty Harold <elharo@ibiblio.org>
2026-08-28 12:00:51 -0400

    javadoc

--- src/main/java/org/apache/maven/plugin/compiler/Options.java
+++ src/main/java/org/apache/maven/plugin/compiler/Options.java
@@ -306,14 +306,15 @@ public final class Options {
      * If not, a warning is logged if {@code immediate} is {@code true}, or stored in the
      * {@link #warning} field if {@code immediate} is {@code false}.
      *
-     * <p>If a message is stored in {@link #warning}, then it will always end with a dot.
-     * This guarantee allows callers to delete the last character and replace it by a coma
+     * <p>The message stored in {@link #warning} always ends with a dot.
+     * This guarantee allows callers to delete the last character and replace it with a comma
      * for continuing the sentence.</p>
      *
      * @param option the option to validate
      * @param count the number of arguments that the caller wants to provide
      * @param immediate whether to log immediately or to store the message in {@link #warning}
-     * @return whether the given option is supported and accepts the specified number of arguments
+     * @return true if the given option is supported and accepts the specified number of arguments,
+     *     false otherwise
      */
     private boolean checkNumberOfArguments(String option, int count, boolean immediate) {
         int expected = checker.isSupportedOption(option);
